b40a8ae6c9e6e8a7fc303e1e79a42648eee2184e
poojakanawte <63844297+poojakanawte@users.noreply.github.com>
2021-05-15 23:14:47 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Patterns{
+public static void main(String args[])
+{
+
+int i,j;
+
+for(i=0;i<=4;i++)
+{
+for(j=0;j<=4;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
